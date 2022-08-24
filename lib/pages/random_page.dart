ca2ca5b312aac828a858c09ea8c799366e08ba92
sittidetP <sittidet@odds.team>
2022-08-24 09:12:27 +0700

    add flex

--- lib/pages/random_page.dart
+++ lib/pages/random_page.dart
@@ -47,35 +47,37 @@ class _RandomPageState extends State<RandomPage> {
       body: SafeArea(
         child: Row(
           children: [
-            Column(
-              children: [
-                Container(
-                  margin: const EdgeInsets.all(16.0),
-                  padding: EdgeInsets.all(20.0),
-                  decoration: const BoxDecoration(
-                    color: Colors.orangeAccent,
-                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  ),
-                  width: MediaQuery.of(context).size.width * 0.6,
-                  constraints: const BoxConstraints(maxHeight: 500),
-                  height: 500,
-                  child: SingleChildScrollView(
-                    controller: _scrollControllerResult,
-                    child: Column(
-                      children: [
-                        for (int i = 0; i < _results.length; ++i)
-                          Text(
-                            "${i + 1}. ${_results[i]}",
-                            style: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 24.0,
-                            ),
-                          ),
-                      ],
+            Flexible(
+              flex: 2,
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(16.0),
+                    padding: EdgeInsets.all(20.0),
+                    decoration: const BoxDecoration(
+                      color: Colors.orangeAccent,
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
                     ),
-                  ),
-                )
-              ],
+                    width: MediaQuery.of(context).size.width * 0.6,
+                    constraints: const BoxConstraints(maxHeight: 500),
+                    child: SingleChildScrollView(
+                      controller: _scrollControllerResult,
+                      child: Column(
+                        children: [
+                          for (int i = 0; i < _results.length; ++i)
+                            Text(
+                              "${i + 1}. ${_results[i]}",
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 24.0,
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
             _buildInputPanel(),
           ],
@@ -86,61 +88,64 @@ class _RandomPageState extends State<RandomPage> {
 
   Widget _buildInputPanel() {
     //ที่ใส่เลข
-    return Column(
-      children: [
-        ElevatedButton(
-          onPressed: () {
-            //เมื่อกดปุ่ม
-            _checkInputAndProcess();
-          },
-          child: const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text(
-              'Random!',
-              style: TextStyle(fontSize: 22.0, color: Colors.yellow),
+    return Flexible(
+      flex: 1,
+      child: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              //เมื่อกดปุ่ม
+              _checkInputAndProcess();
+            },
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Random!',
+                style: TextStyle(fontSize: 22.0, color: Colors.yellow),
+              ),
             ),
           ),
-        ),
-        Container(
-          margin: const EdgeInsets.all(16.0),
-          decoration: const BoxDecoration(
-            color: Colors.indigo,
-            borderRadius: BorderRadius.all(Radius.circular(20.0)),
-          ),
-          width: MediaQuery.of(context).size.width * 0.35,
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Container(
-              constraints: const BoxConstraints(maxHeight: 500),
-              child: SingleChildScrollView(
-                controller: _scrollControllerInput,
-                child: TextField(
-                  //ช่องใส่ชื่อ
-                  controller: _controller,
-                  keyboardType: TextInputType.multiline,
-                  maxLines: null,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 24.0,
+          Container(
+            margin: const EdgeInsets.all(16.0),
+            decoration: const BoxDecoration(
+              color: Colors.indigo,
+              borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            ),
+            width: MediaQuery.of(context).size.width * 0.35,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Container(
+                constraints: const BoxConstraints(maxHeight: 500),
+                child: SingleChildScrollView(
+                  controller: _scrollControllerInput,
+                  child: TextField(
+                    //ช่องใส่ชื่อ
+                    controller: _controller,
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                    ),
+                    cursorColor: Colors.white,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                          color: Colors.white.withOpacity(0.5),
+                        )),
+                        hintText: 'Enter the Names here',
+                        hintStyle: TextStyle(
+                          color: Colors.white.withOpacity(0.5),
+                          fontSize: 20.0,
+                        )),
                   ),
-                  cursorColor: Colors.white,
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                      enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                        color: Colors.white.withOpacity(0.5),
-                      )),
-                      hintText: 'Enter the Names here',
-                      hintStyle: TextStyle(
-                        color: Colors.white.withOpacity(0.5),
-                        fontSize: 20.0,
-                      )),
                 ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
